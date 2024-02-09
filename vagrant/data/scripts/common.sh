d822efb254ff55b9d803ef3c6ccfcfa6281585fc
jtorrex <jose.torres@hpcnow.com>
2024-02-09 16:42:16 +0100

    common: configure /etc/hosts + authorized_key

--- vagrant/data/scripts/common.sh
+++ vagrant/data/scripts/common.sh
@@ -4,10 +4,14 @@ sudo apt-get update
 sudo apt-get install -y vim apt-transport-https ca-certificates nfs-common
 
 # configure name resolution
-echo "" >> /etc/hosts
-echo "# Dev Cluster" >> /etc/hosts
-echo "10.10.10.100    head" >> /etc/hosts
-echo "10.10.10.101    node01" >> /etc/hosts
-echo "10.10.10.102    node02" >> /etc/hosts
-echo "10.10.10.103    node03" >> /etc/hosts
-echo "10.10.10.110    storage" >> /etc/hosts
+sudo echo "" >> /etc/hosts
+sudo echo "# Dev Cluster" >> /etc/hosts
+sudo echo "10.10.10.100    head" >> /etc/hosts
+sudo echo "10.10.10.101    node01" >> /etc/hosts
+sudo echo "10.10.10.102    node02" >> /etc/hosts
+sudo echo "10.10.10.103    node03" >> /etc/hosts
+sudo echo "10.10.10.110    storage" >> /etc/hosts
+
+# configure root key
+sudo mkdir -p $HOME/.ssh
+sudo cat /data/config/cluster-ssh-key.pub >> $HOME/.ssh/authorized_keys
