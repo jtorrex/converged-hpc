75a4931e57ded35f663ef56bae3efb4fcbaaa3bb
jtorrex <jose.torres@hpcnow.com>
2024-02-09 16:43:06 +0100

    nodes: install workers from token

--- vagrant/data/scripts/node-k8s.sh
+++ vagrant/data/scripts/node-k8s.sh
@@ -2,7 +2,7 @@
 echo 'Installing k0s'
 curl -sSLf https://get.k0s.sh | sudo sh
 mkdir -p /etc/k0s
-sudo k0s install worker --token-file /data/config/worker-token.txt
+sudo k0s install worker --token-file /data/config/k8s/worker-token.txt
 
 echo 'Starting k0s'
 sudo k0s start
