9be427a3c1c7bad5b48dea0258e9666680dd5d52
jtorrex <jose.torres@hpcnow.com>
2024-02-16 15:06:44 +0100

    ansible: force playbook execution as root

--- vagrant/data/scripts/ansible.sh
+++ vagrant/data/scripts/ansible.sh
@@ -2,4 +2,4 @@
 eval $(ssh-agent)
 ssh-add /data/config/cluster-ssh-key
 sudo apt-get install ansible -y
-ansible-playbook -c local -i /data/ansible/inventory.yaml /data/ansible/cluster.yaml
+ansible-playbook -u root -i /data/ansible/inventory.yaml /data/ansible/cluster.yaml
