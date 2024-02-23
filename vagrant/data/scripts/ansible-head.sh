8ff1023f1dd43873d13b96057ad179322966a265
jtorrex <jose.torres@hpcnow.com>
2024-02-23 14:36:56 +0100

    ansible: added scripts for head and nodes

---
+++ vagrant/data/scripts/ansible-head.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+eval $(ssh-agent)
+ssh-add /data/config/cluster-ssh-key
+sudo apt-get install ansible -y
+ansible-playbook -u root -i /data/ansible/inventory.yaml /data/ansible/head.yaml -v
