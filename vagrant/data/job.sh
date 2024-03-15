c03944e2c857e57175dbb363a75f087dddc2c881
jtorrex <jose.torres@hpcnow.com>
2024-03-15 13:07:29 +0100

    slurm: added basic sample jobscript

--- vagrant/data/job.sh
+++ vagrant/data/job.sh
@@ -5,8 +5,8 @@
 #SBATCH --partition=cluster
 
 #SBATCH --time=2:00
-#SBATCH --ntasks=2
-#SBATCH --nodes=2
+#SBATCH --ntasks=3
+#SBATCH --nodes=3
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
 
